1edde647ab1395d859015704e79228039b9c213e
PM-Primo <pierrem.primo@gmail.com>
2022-11-25 15:57:07 +0100

    added list of sessions

--- src/Controller/SessionFormationController.php
+++ src/Controller/SessionFormationController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\SessionFormation;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SessionFormationController extends AbstractController
 {
     /**
      * @Route("/session/formation", name="app_session_formation")
      */
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+
+        $sessions_formation  = $doctrine->getRepository(SessionFormation::class)->findBy([],["date_debut"=>"ASC"] );
+
+
         return $this->render('session_formation/index.html.twig', [
-            'controller_name' => 'SessionFormationController',
+            'sessions_formation' => $sessions_formation
         ]);
     }
 }
